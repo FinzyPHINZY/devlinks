6f83b1cfdb086c9e0ed71932cb52b9e396cd110f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-11 21:26:09 +0100

    Fixed styling issue by adding justify-center to the LogoutButton component for better alignment.

--- src/components/Buttons/LogoutButton.jsx
+++ src/components/Buttons/LogoutButton.jsx
@@ -7,7 +7,7 @@ import { signOut } from "next-auth/react";
 const LogoutButton = ({ className }) => {
   return (
     <button
-      className={`flex items-center gap-2 border px-4 py-2 shadow ${className}`}
+      className={`flex justify-center items-center gap-2 border px-4 py-2 shadow ${className}`}
       onClick={() => signOut()}
     >
       <span>Logout</span>
